3e8163f9a96e7993f5d9d78772f014b7473a501e
RodrigoTAbreu <rodrigo.telles.abreu@gmail.com>
2020-09-14 00:43:25 -0300

    Subindo Arquivos

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*Aula 04 */ 
+window.alert('Minha primeira MSG') /*Alerta ao carregar a tela */
+window.confirm('Está gostando de Java Script ?') /*MSG com a opção de CONFIRMAR em que o usuário deve clicar em OK ou CANCELAR */
+var nome = window.prompt('Qual é o seu nome ?') /*fazendo com que a variavel NOME receba o que foi digitado por meio do comando WINDOW.PROMPT */
+window.alert('Seja bem vindo(a) '+ nome)/*Monstrando o conteúdo da variável NOME, digitada anteriormente.*/
