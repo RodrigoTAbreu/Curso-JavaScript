27814d3096e5bb095acd5a58b3b9c08f144e02dc
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-09-16 06:15:18 -0300

    aula11 e 12

---
+++ Aula11/ex008-2.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do carro é ${vel}km/h`)
+    if (vel > 60){ //CONDIÇÃO SIMPLES
+        console.log(`Você ultapassou o limite de velocidade. MULTADO!!`)
+    } console.log(`Dirija sempre com cuidado.`)
+    
