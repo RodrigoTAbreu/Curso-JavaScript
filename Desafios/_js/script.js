1acc387055b708a8df04ac2b3d8cd0cea927bd5c
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2020-09-18 06:38:05 -0300

    Desafio 02

--- Desafios/_js/script.js
+++ Desafios/_js/script.js
@@ -6,23 +6,28 @@ function carregar(){
     var p = document.querySelector('div#periodo')
     
                 
-    if (hora <12) {
+    if (hora>5 && hora <12) {
         msg.innerHTML += `Agora são exatamente <strong>${hora}hrs ${minuto}m</strong>.`
         msg.innerHTML += `BOM DIA!`
-        img.src = "manha.jpg"
+        img.src = "img/manha.jpg"
         document.body.style.background = '#FE9500'        
         
     }else if (hora >=12 && hora <19) {
         msg.innerHTML += `Agora são exatamente <strong>${hora}hrs ${minuto}m</strong>.`
         msg.innerHTML += `BOA TARDE!`
-        img.src="tarde.jpg"
+        img.src="img/tarde.jpg"
         document.body.style.background = '#C3DEEF'
         document.body.style.color = 'black'
-    }else {
+    }else if (hora>= 19 && hora <23){
         msg.innerHTML += `Agora são exatamente <strong>${hora}hrs ${minuto}m</strong>.`
         msg.innerHTML += `BOA NOITE!`
-        img.src="noite.jpg"
+        img.src="img/noite.jpg"
         document.body.style.background = '#2D5493'
 
+    }else if(hora <=5){
+        msg.innerHTML += `Agora são exatamente <strong>${hora}hrs ${minuto}m</strong>.`
+        msg.innerHTML += `BOA MADRUGA!!!`
+        img.src="img/madrugada.jpg"
+        document.body.style.background = '#4C5A63'
     }
 }
---
+++ Desafios/_js/script2.js
@@ -0,0 +1,35 @@
+function verificar(){ // função ONCLICK do botão
+    var data = new Date() //variavel recebe conteúdo DATA do computador
+    var ano = data.getFullYear() // variavel ANO recebe apenas o ano completo FULLYEAR
+    var fano = document.getElementById('txtano') //variavel recebe o valor do ID txtano do input  
+    var res = document.querySelector('div#resultado') //variavel recebe o conteudo da DIV Resultado
+    
+    if (fano.value.length == 0 || Number(fano.value)> ano){ //se o tamanho(length) em seu valor (value) da variavel fano for igual (==) a 0 ou (||) o valor (value) de fano em numero (Number) for maior que ano
+        window.alert(`[ERRO] Dados incorretos.`) //gera um alerta
+        
+    }else{ //se nao gera outro alerta.
+        var fsex = document.getElementsByName ('radsex') // pegando o valor do input radio
+        var idade = ano - Number(fano.value) // subtraindo o valor de ano por fano
+        var genero = '' // criando a variavel em branco
+        var img = document.createElement('img')// ---->>> criando uma TAG IMG
+        img.setAttribute('id','foto') // atribuindo uma ID com nome FOTO para a tag IMG 
+
+            if (fsex[0].checked){ //se a variavel FSEX na sua posição [0](desmarcada)checked
+                genero = 'HOMEM' //variavel genero assume o valor HOMEM
+                if (idade>=0 && idade<10){
+                    img.setAttribute('src','img/cboy.jpg')
+                }else if(idade <21){
+                    img.setAttribute('src','img/cmenina.jpg')
+                }else if (idade< 50){
+                    img.setAttribute('src','')
+                }else{
+                    img.setAttribute('src','')
+                }
+            } else if(fsex[1].checked){ //se a variavel FSEX na sua posição [1] estiver marcada (checked )
+                genero ='MULHER' //variavel genero assume MULHER
+            }
+        res.style.textAlign = 'center' //alinhando o texto de RES com java
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos. ` //imprimindo o resultado na tela.
+       
+    }
+}
